1130e2f156931e2e335e9fcfac8ec087accb97b3
Snehaandiyappan <41100598+Snehaandiyappan@users.noreply.github.com>
2018-07-12 10:25:04 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,17 @@
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int b=-3;
+		if(b%2==0){
+			System.out.println("Even");
+		}
+		else if(b<0){
+			System.out.println("invalid");
+		}
+		else{
+			System.out.println("odd");
+		}
+	}
+}
