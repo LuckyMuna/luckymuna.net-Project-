86d29ad7e0c57297c4fe3846d2c75fe4ce315cf4
Snehaandiyappan <41100598+Snehaandiyappan@users.noreply.github.com>
2018-07-14 16:00:05 +0530

    Create alpa.java

---
+++ alpa.java
@@ -0,0 +1,12 @@
+public class Alpabet {
+	public static void main(String[]args){
+	char ch;
+	Scanner sc=new Scanner(System.in);
+	 ch= sc.next().charAt(0);
+	  if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+         System.out.println(" alphabet.");
+     
+	else
+         System.out.println(" not");
+	}
+}
