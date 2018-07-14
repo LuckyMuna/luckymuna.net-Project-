2b107137d2c29a4951d60cee1b24fc136ed3424f
Snehaandiyappan <41100598+Snehaandiyappan@users.noreply.github.com>
2018-07-14 15:41:41 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,12 @@
+public class Alpabet {
+	public static void main(String[]args){
+	char c;
+	Scanner sc=new Scanner(System.in);
+	 c= sc.next().charAt(0);
+	  if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+         System.out.println(" alphabet.");
+     
+	else
+         System.out.println(" not");
+	}
+}
