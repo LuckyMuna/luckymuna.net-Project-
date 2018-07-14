de0248ce6a5d85d28a40cfe9aa9434c495ba68fc
Snehaandiyappan <41100598+Snehaandiyappan@users.noreply.github.com>
2018-07-14 15:31:48 +0530

    Create vowelss.java

---
+++ vowelss.java
@@ -0,0 +1,28 @@
+public class vowels {
+	public static void main(String[] args){
+int i=0;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a character : ");
+char ch=sc.next( ).charAt(0);
+switch(ch)
+{
+ case  'a' :
+ case 'e'  :
+ case 'i'   :
+ case 'o'  :
+ case 'u'  :
+ case  'A' :
+ case 'E'  :
+ case 'I'   :
+ case 'O'  :
+ case 'U'  :i++;
+}
+if(i==1)
+System.out.println(" Vowel");
+else 
+	if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+	System.out.println("Consonent");
+	else
+	System.out.println("in valid");		
+}
+}
