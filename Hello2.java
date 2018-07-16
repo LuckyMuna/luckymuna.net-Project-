6936ee49a1037bbb035c0eb2733bb8d6d5532132
Snehaandiyappan <41100598+Snehaandiyappan@users.noreply.github.com>
2018-07-16 10:16:41 +0530

    Create Hello2.java

---
+++ Hello2.java
@@ -0,0 +1,16 @@
+public class hello2 {
+       public static void main(String[]args){
+    	   String s;
+    	   int n;
+    	   Scanner sc=new Scanner(System.in);
+    	   s=sc.nextLine();
+    	   n=sc.nextInt();
+    	   for(int i=0;i<n;i++){
+    		   System.out.println(s);
+    		   
+    		   
+    	   }
+    	   
+    	   
+       }
+}
