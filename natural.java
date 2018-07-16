9e674b1e80736f60d4adea458e17d42e9ea81346
Snehaandiyappan <41100598+Snehaandiyappan@users.noreply.github.com>
2018-07-16 10:24:31 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,12 @@
+public class naturalNum {
+	public static void main(String[]args){
+		int n,count=0;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		for(int i=0;i<n+1;i++){
+			count++;
+			}
+		System.out.println(count);
+	}
+
+}
