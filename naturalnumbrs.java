2a0695ac70eee2327908401e49763bb0b6ba7c94
Snehaandiyappan <41100598+Snehaandiyappan@users.noreply.github.com>
2018-07-16 11:25:24 +0530

    Create naturalnumbrs.java

---
+++ naturalnumbrs.java
@@ -0,0 +1,15 @@
+public class ArraynK {
+	public static void main(String[]args){
+  int n,sum=0,count=0;
+	Scanner sc=new Scanner(System.in);
+	n=sc.nextInt();
+	while(count<=n){
+		sum=sum+count;
+		count++;
+	
+		}
+	System.out.println(sum);
+
+
+}
+}
