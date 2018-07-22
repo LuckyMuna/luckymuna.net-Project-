fc8d8d33e07c69fa899aaea8d9928bb04dc37474
Snehaandiyappan <41100598+Snehaandiyappan@users.noreply.github.com>
2018-07-22 14:17:38 +0530

    Create intervalprime.java

---
+++ intervalprime.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class palindrome {
+	public static void main(String[]args){
+		
+		 Scanner in = new Scanner(System.in);
+	     int a=in.nextInt();
+	     int b=in.nextInt();
+	     int count=0,num;
+	     for(int i=a;i<b;i++){
+	    	 for(num=2;num<i;num++){
+	    		 if(i%num==0){
+	    			 count=0;
+	    			 break;
+	    		 }else{
+	    			 count=1;
+	    		 }
+	    	 }
+	    			 if(count==1){
+	    			 System.out.println(i);
+	    			 }
+	    			
+	    		 }
+	     in.close();
+	    	 }
+	    }
+
+		
