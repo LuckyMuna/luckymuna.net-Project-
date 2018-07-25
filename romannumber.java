66b643bbd97dcbebdfe524e4d3b2d1d0145b8b3b
Snehaandiyappan <41100598+Snehaandiyappan@users.noreply.github.com>
2018-07-25 09:51:32 +0530

    Update romannumber.java

--- romannumber.java
+++ romannumber.java
@@ -1,57 +1,16 @@
 import java.util.Scanner;
 public class string {
 	public static void main(String[]args){
-		Scanner sc=new Scanner(System.in);
-        int a;
-        a=sc.nextInt();
-        switch(a){
-        case 1:{
-        	System.out.println("I");
-        	break;
-        }
-        case 2 :{
-        	System.out.println("II");
-        	break;
-        }
-        case 3:{
-        	System.out.println("III");
-        	break;
-        }
-        case 4:{
-        	System.out.println("IV");
-        	break;
-        }
-        case 5:{
-        	System.out.println("V");
-        	break;
-        }
-        case 6:{
-        	System.out.println("VI");
-        	break;
-        }
-        case 7:{
-        	System.out.println("VII");
-        	break;
-        }
-        case 8:{
-        	System.out.println("VIII");
-        	break;
-        }
-        case 9:{
-        	System.out.println("IX");
-        	break;
-        }
-        case 10:{
-        	System.out.println("X");
-        	break;
-        }
-        case 11:{
-        	System.out.println("XI");
-        	break;
-        }
-        case 12:{
-        	System.out.println("XII");
-        	break;
-        }
-        
-        }
+        Scanner sc=new Scanner(System.in);
+		String a=sc.next();
+		int b[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+		String c[]={"I","II","III","IV","V","VI","VII","VIII","IX","X"};
+		int i=0;
+		for(i=0;i<10;i++){
+			if(a.equals(c[i])){
+				System.out.println(b[i]);
+	}
+
+}
+	}
+}
