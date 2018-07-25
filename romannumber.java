113aa150be6310e9fd18804def91f2ba89316f11
Snehaandiyappan <41100598+Snehaandiyappan@users.noreply.github.com>
2018-07-25 09:21:55 +0530

    Create romannumber.java

---
+++ romannumber.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+public class string {
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+        int a;
+        a=sc.nextInt();
+        switch(a){
+        case 1:{
+        	System.out.println("I");
+        	break;
+        }
+        case 2 :{
+        	System.out.println("II");
+        	break;
+        }
+        case 3:{
+        	System.out.println("III");
+        	break;
+        }
+        case 4:{
+        	System.out.println("IV");
+        	break;
+        }
+        case 5:{
+        	System.out.println("V");
+        	break;
+        }
+        case 6:{
+        	System.out.println("VI");
+        	break;
+        }
+        case 7:{
+        	System.out.println("VII");
+        	break;
+        }
+        case 8:{
+        	System.out.println("VIII");
+        	break;
+        }
+        case 9:{
+        	System.out.println("IX");
+        	break;
+        }
+        case 10:{
+        	System.out.println("X");
+        	break;
+        }
+        case 11:{
+        	System.out.println("XI");
+        	break;
+        }
+        case 12:{
+        	System.out.println("XII");
+        	break;
+        }
+        
+        }
